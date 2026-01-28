4edeb9f0ed486ad2347b0e4536c532af3b83149b
SakibSibly <sibly@duck.com>
2026-01-28 17:50:20 +0600

    Add "Initial project structure with DRF and JWT created"

---
+++ api/v1/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    # OpenAPI schema
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+
+    # Swagger UI
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
+    # ReDoc
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    # JWT
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
