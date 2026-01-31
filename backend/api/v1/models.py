866e7fe0c3d44cf3a546647da83afede111d58c1
SakibSibly <sibly@duck.com>
2026-01-31 15:28:05 +0600

    Update "Author associated with blog"

--- backend/api/v1/models.py
+++ backend/api/v1/models.py
@@ -42,6 +42,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
